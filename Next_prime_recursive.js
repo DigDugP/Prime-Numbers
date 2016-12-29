329bfd71d10373eabfde4a177ebf9f944a83e432
DigDugP <doug@perucca.email>
2016-12-29 13:28:51 -0900

    Update Next_prime_recursive.js

--- Next_prime_recursive.js
+++ Next_prime_recursive.js
@@ -1,3 +1,5 @@
+/* playing around with recursion - go to: https://github.com/DigDugP/Prime-Numbers/blob/master/Prime_Page.html to try on a page */
+
 function nextprime(currentprime){
 	currentprime++;
 	for	(i = 2; i < currentprime; i++){
